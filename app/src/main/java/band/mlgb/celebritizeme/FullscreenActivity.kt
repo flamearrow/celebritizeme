d3414747cdd799d355218b20e23a6f33e0fd89fc
Chen Cen <arrow.cen@gmail.com>
2020-05-03 18:56:44 -0700

    hook up image picker

--- app/src/main/java/band/mlgb/celebritizeme/FullscreenActivity.kt
+++ app/src/main/java/band/mlgb/celebritizeme/FullscreenActivity.kt
@@ -1,10 +1,16 @@
 package band.mlgb.celebritizeme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.features.ReturnMode
 import kotlinx.android.synthetic.main.activity_fullscreen.*
 
 /**
@@ -12,113 +18,80 @@ import kotlinx.android.synthetic.main.activity_fullscreen.*
  * status bar and navigation/system bar) with user interaction.
  */
 class FullscreenActivity : AppCompatActivity() {
-    private val mHideHandler = Handler()
-    private val mImagePickerHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        fullscreen_content.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    }
-    private val mShowPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-        fullscreen_content_controls.visibility = View.VISIBLE
-    }
-    private var mVisible: Boolean = false
-    private val mHideRunnable = Runnable { hide() }
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val mDelayHideTouchListener = View.OnTouchListener { _, _ ->
-        //        if (AUTO_HIDE) {
-//            delayedHide(AUTO_HIDE_DELAY_MILLIS)
-//        }
-        mImagePickerHandler.post()
-        {
-            Toast.makeText(applicationContext, "heyyo", Toast.LENGTH_LONG).show()
-
-        }
-
-
-
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_fullscreen)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        mVisible = true
-
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreen_content.setOnClickListener { toggle() }
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        dummy_button.setOnTouchListener(mDelayHideTouchListener)
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100)
-    }
+    fun celebritize(view: View) {
+        Toast.makeText(this, "celebritize", Toast.LENGTH_SHORT).show()
 
-    private fun toggle() {
-        if (mVisible) {
-            hide()
-        } else {
-            show()
+        Intent(this, ResultActivity::class.java).let {
+            val bitmap: Bitmap? = null
+            it.putExtra(RESULT_IMAGE, "MLGB")
+            startActivity(it)
         }
+
     }
 
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-        fullscreen_content_controls.visibility = View.GONE
-        mVisible = false
+    fun pickcustom(view: View) {
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable)
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+        ImagePicker.create(this)
+            .returnMode(ReturnMode.ALL) // set whether pick and / or camera action should return immediate result or not.
+//            .folderMode(true) // folder mode (false by default)
+            .toolbarFolderTitle("Folder") // folder selection title
+            .toolbarImageTitle("Tap to select") // image selection title
+//            .toolbarArrowColor(Color.BLACK) // Toolbar 'up' arrow color
+//            .includeVideo(true) // Show video on image picker
+            .single() // single mode
+//            .multi() // multi mode (default mode)
+//            .limit(10) // max images can be selected (99 by default)
+//            .showCamera(true) // show camera or not (true by default)
+//            .imageDirectory("Camera") // directory name for captured image  ("Camera" folder by default)
+//            .origin(images) // original selected images, used in multi mode
+//            .exclude(images) // exclude anything that in image.getPath()
+//            .excludeFiles(files) // same as exclude but using ArrayList<File>
+//            .theme(R.style.CustomImagePickerTheme) // must inherit ef_BaseTheme. please refer to sample
+//            .enableLog(false) // disabling log
+            .start() // start image picker activity with request code
     }
 
-    private fun show() {
-        // Show the system bar
-        fullscreen_content.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        mVisible = true
+    fun pickceleb(view: View) {
 
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable)
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+        ImagePicker.create(this)
+            .returnMode(ReturnMode.ALL) // set whether pick and / or camera action should return immediate result or not.
+            .folderMode(true) // folder mode (false by default)
+            .toolbarFolderTitle("Folder") // folder selection title
+            .toolbarImageTitle("Tap to select") // image selection title
+            .toolbarArrowColor(Color.BLACK) // Toolbar 'up' arrow color
+//            .includeVideo(true) // Show video on image picker
+            .single() // single mode
+//            .multi() // multi mode (default mode)
+            .limit(10) // max images can be selected (99 by default)
+//            .showCamera(true) // show camera or not (true by default)
+//            .imageDirectory("Camera") // directory name for captured image  ("Camera" folder by default)
+//            .origin(images) // original selected images, used in multi mode
+//            .exclude(images) // exclude anything that in image.getPath()
+//            .excludeFiles(files) // same as exclude but using ArrayList<File>
+//            .theme(R.style.CustomImagePickerTheme) // must inherit ef_BaseTheme. please refer to sample
+            .enableLog(false) // disabling log
+            .start() // start image picker activity with request code
     }
 
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private fun delayedHide(delayMillis: Int) {
-        mHideHandler.removeCallbacks(mHideRunnable)
-        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            ImagePicker.getFirstImageOrNull(data)?.let {
+                val bitmap = BitmapFactory.decodeFile(it.path)
+                val roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(resources, bitmap)
+                roundedBitmapDrawable.cornerRadius = bitmap.width.coerceAtMost(bitmap.height) / 2.0f
+                custom_image.setImageDrawable(roundedBitmapDrawable)
+//                custom_image.setImageBitmap(BitmapFactory.decodeFile(it.path))
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     companion object {
@@ -134,15 +107,7 @@ class FullscreenActivity : AppCompatActivity() {
          */
         private val AUTO_HIDE_DELAY_MILLIS = 3000
 
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private val UI_ANIMATION_DELAY = 300
+        const val RESULT_IMAGE = "RESULT_IMAGE"
 
-        /**
-         * Pick up an image from local gallery.
-         */
-        private val REQUEST_CODE_CHOOSE = 1
     }
 }
---
+++ app/src/main/java/band/mlgb/celebritizeme/ResultActivity.kt
@@ -0,0 +1,27 @@
+package band.mlgb.celebritizeme
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        intent?.let {
+            Toast.makeText(
+                this, it.getStringExtra(FullscreenActivity.RESULT_IMAGE)
+                , Toast.LENGTH_LONG
+            ).show()
+        }
+
+    }
+
+
+    fun share(view: View) {
+
+        Toast.makeText(this, "share", Toast.LENGTH_SHORT).show()
+
+    }
+}
